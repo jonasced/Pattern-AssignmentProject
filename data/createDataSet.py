cfd11440362ea56a7c1d951b2ddfbabccbc3d149
serkanardaa <arda.yilal@ozu.edu.tr>
2021-05-18 16:24:42 +0200

    createDataSet now saves labels of data as well

--- data/createDataSet.py
+++ data/createDataSet.py
@@ -7,7 +7,7 @@ from CharacterFeatureExtractor import *
 
 num_char = 5
 num_sample = 5
-char_strings = ["A", "C", "K", "P", "X"]
+char_labels = ["A", "C", "K", "P", "X"]
 current_path = os.path.dirname(os.path.abspath(__file__)) 
 list_of_char_lists = []
 list_of_feature_lists = []
@@ -17,7 +17,7 @@ for c in range(num_char):
     temp_char_list = []
     temp_feature_list =[]
     for s in range(num_sample):
-        char = np.load(os.path.join(current_path, char_strings[c] + str(s + 1) + ".npy") )
+        char = np.load(os.path.join(current_path, char_labels[c] + str(s + 1) + ".npy") )
         feature,sampled_char = featureExtractor(char,thr, False)
         temp_char_list.append(char)
         temp_feature_list.append(feature)
@@ -33,13 +33,15 @@ if decision == "y":
 
     file_path_char = os.path.join(current_path, name + ".cdb")
     file_path_feauture = os.path.join(current_path, name + "_features.cdb")
+    file_path_labels = os.path.join(current_path, name + "_labels.cdb")
     
     with open(file_path_char, "wb") as fp: #Pickling
         pickle.dump(list_of_char_lists, fp)
     with open(file_path_feauture, "wb") as fp: #Pickling
         pickle.dump(list_of_feature_lists, fp)   
+    with open(file_path_labels, "wb") as fp: #Pickling
+        pickle.dump(char_labels, fp)   
     
-
 
 
 
