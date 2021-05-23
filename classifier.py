560a76efbcc10ff814aab25d323c40445dd6b918
Jonas Cederberg <jonas.cederberg@gmail.com>
2021-05-23 22:58:38 +0200

    Moved comments

--- classifier.py
+++ classifier.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-def classifier(HMM_Models, labels, sample):
+def classifier(HMM_Models, labels, sample, useprint=False):
     """
     Inputs:
         1) List of Class Models
@@ -20,8 +20,15 @@ def classifier(HMM_Models, labels, sample):
         lprob = np.sum(np.array(clog))
         # lprob = np.exp(lprob)
         class_probs[char] = lprob
-    
-    index_class = np.argmax(class_probs)
-    
+
+    index_class = np.nanargmax(class_probs)
+
+    if useprint:
+        print("\n---START OF SAMPLE---")
+        print("Classified as:", labels[index_class])
+        for i in range(len(labels)):
+            print("P(input) = ", labels[i], ": ", class_probs[i])
+        print("\n")
+
     return labels[index_class]
 
